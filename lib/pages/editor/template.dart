2d2ea1e0afd5ab04d0292f0df974ed35c5c3a779
JacobMartinage <62039158+JacobMartinage@users.noreply.github.com>
2025-09-27 09:20:39 -0400

    json temp

---
+++ lib/pages/editor/template.dart
@@ -0,0 +1,12 @@
+// A simple starter delta (JSON-compatible map)
+const List<Map<String, dynamic>> kStarterDelta = [
+    {"insert": "SecondStudent\n", "attributes": {"header": 1}},
+    {"insert": "Type / to open the command menu.\n"},
+    {"insert": "\n"},
+    {"insert": "• Try a bullet list\n"},
+    {"insert": "1. Or a numbered list\n"},
+    {"insert": "\n"},
+    {"insert": "``` code block ```\n", "attributes": {"code-block": true}},
+    {"insert": "\n"},
+  ];
+  
