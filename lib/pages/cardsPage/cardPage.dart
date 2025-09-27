2c41223ea895e1e51f08f5d9e6c233e512926bc9
Emmet-Ledell <emmet.ledell@gmail.com>
2025-09-27 13:50:16 -0400

    card page

--- lib/pages/cardsPage/cardPage.dart
+++ lib/pages/cardsPage/cardPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flip_card/flutter_flip_card.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -27,21 +29,34 @@ class CardData {
   }
 }
 
-Future<void> hitApi() async {
-  var client = http.Client();
+Future<List<dynamic>> fetchData() async {
+  final url = Uri.parse(
+      '${localStorage.getItem('canvasDomain')}/api/v1/courses?include[]=&state[]=available');
+  print(url);
+
+  final headers = {
+    'Authorization': 'Bearer ${localStorage.getItem('canvasToken')}',
+  };
+  print(headers);
+
   try {
-    var response = await client.post(Uri.https('{{canvas_domain}}/api/v1/courses?include[]=&state[]=available', 'whatsit/create'),
-        body: {'name': 'doodle', 'color': 'blue'});
-    var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
-    var uri = Uri.parse(decodedResponse['uri'] as String);
-    print(await client.get(uri));
-  } finally {
-    client.close();
+    final response = await http.get(url, headers: headers);
+
+    if (response.statusCode == 200) {
+      print(response.body);
+
+      return jsonDecode(response.body);
+    }
+    print("Error: ${response.statusCode}");
+    return [];
+  } catch (e) {
+    print("Error: $e");
+    return [];
   }
 }
 
-Widget cardCarouselWidget(
-    String frontText, String backText, BuildContext context) {
+Widget cardCarouselWidget(String frontText, String backText,
+    BuildContext context, List<dynamic> data) {
   final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
   final cardFlipper = FlipCardController();
@@ -107,7 +122,7 @@ Widget cardCarouselWidget(
       padding: const EdgeInsets.all(8.0),
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 5,
+        itemCount: data.length,
         itemBuilder: (context, index) => cardWidget(
           frontText,
           backText,
@@ -119,10 +134,21 @@ Widget cardCarouselWidget(
 }
 
 class _MyAppState extends State<CardPage> {
+  var data = [];
   final con = FlipCardController();
 
   @override
+  void initState() {
+    super.initState();
+    fetchData().then((value) {
+      setState(() {
+        data = value;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return cardCarouselWidget("frontText", "backText", context);
+    return cardCarouselWidget("frontText", "backText", context, data);
   }
 }
