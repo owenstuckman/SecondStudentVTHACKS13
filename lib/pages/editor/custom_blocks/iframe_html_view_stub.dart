9727aee496311f3142459605a1af264e12031f89
JacobMartinage <62039158+JacobMartinage@users.noreply.github.com>
2025-09-28 01:59:15 -0400

    Delete iframe_html_view_stub.dart

--- lib/pages/editor/custom_blocks/iframe_html_view_stub.dart
+++
@@ -1,4 +0,0 @@
-// Non-web stub so the conditional import resolves on mobile/desktop.
-import 'package:flutter/widgets.dart';
-
-Widget buildHtmlIFrame(String url, double height) => const SizedBox.shrink();
